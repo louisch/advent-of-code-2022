ab0f99a791489d67064ff0e537c1741e09832b00
Louis Chan <louis.chan@lidl.com.hk>
2022-12-07 08:00:11 +0300

    Solve day 4

---
+++ day4/day4.go
@@ -0,0 +1,52 @@
+package day4
+
+import (
+    "fmt"
+    "strings"
+
+    "github.com/louisch/advent-of-code-2022/util"
+)
+
+func getRanges(line string) (int, int, int, int) {
+    range1, range2, _ := strings.Cut(line, ",")
+    range1LowerStr, range1UpperStr, _ := strings.Cut(range1, "-")
+    range2LowerStr, range2UpperStr, _ := strings.Cut(range2, "-")
+    range1Lower := util.ParseIntSimple(range1LowerStr)
+    range1Upper := util.ParseIntSimple(range1UpperStr)
+    range2Lower := util.ParseIntSimple(range2LowerStr)
+    range2Upper := util.ParseIntSimple(range2UpperStr)
+    return range1Lower, range1Upper, range2Lower, range2Upper
+}
+
+func doRangesIntersectCompletely(lower1 int, upper1 int, lower2 int, upper2 int) bool {
+    return lower1 >= lower2 && upper1 <= upper2 ||
+           lower2 >= lower1 && upper2 <= upper1
+}
+
+func doRangesNotIntersect(lower1 int, upper1 int, lower2 int, upper2 int) bool {
+    return upper1 < lower2 || lower1 > upper2
+}
+
+func Part1(day int, part int) {
+    rangesThatIntersectCompletely := 0
+    visitFile := func(line string) {
+        lower1, upper1, lower2, upper2 := getRanges(line)
+        if (doRangesIntersectCompletely(lower1, upper1, lower2, upper2)) {
+            rangesThatIntersectCompletely++
+        }
+    }
+    util.ScanFileByLine(day, part, visitFile)
+    fmt.Printf("Ranges intersecting completely: %v\n", rangesThatIntersectCompletely)
+}
+
+func Part2(day int, part int) {
+    rangesThatIntersectPartially := 0
+    visitFile := func(line string) {
+        lower1, upper1, lower2, upper2 := getRanges(line)
+        if (!doRangesNotIntersect(lower1, upper1, lower2, upper2)) {
+            rangesThatIntersectPartially++
+        }
+    }
+    util.ScanFileByLine(day, part, visitFile)
+    fmt.Printf("Ranges intersecting partially: %v\n", rangesThatIntersectPartially)
+}
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
     "github.com/louisch/advent-of-code-2022/day1"
     "github.com/louisch/advent-of-code-2022/day2"
     "github.com/louisch/advent-of-code-2022/day3"
+    "github.com/louisch/advent-of-code-2022/day4"
 )
 
 var challenges = map[string]func(int, int){
@@ -17,6 +18,8 @@ var challenges = map[string]func(int, int){
     "2:2": day2.Part2,
     "3:1": day3.Part1,
     "3:2": day3.Part2,
+    "4:1": day4.Part1,
+    "4:2": day4.Part2,
 }
 
 func main() {
